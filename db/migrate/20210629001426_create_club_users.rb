a11c7482ad38569bb2953361c0c2c7aee6569544
Benis Tambe <will.jenifer@yahoo.com>
2021-06-28 20:19:20 -0400

    added join table model and migration. Added devise gem for user model

---
+++ db/migrate/20210629001426_create_club_users.rb
@@ -0,0 +1,11 @@
+class CreateClubUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :club_users do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :club, null: false, foreign_key: true
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
