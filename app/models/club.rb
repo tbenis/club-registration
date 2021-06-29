e431ce695e684c1ffa7c6dc24d01d2bd756e4f10
Benis Tambe <will.jenifer@yahoo.com>
2021-06-28 21:12:16 -0400

    defined relationships in club model and added validations

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,8 @@
 class Club < ApplicationRecord
+    has_many :club_users
+
+    has_many :users, through: :club_users
+    validates :name,  presence: true, uniqueness: true, length: {minimum: 3}
+    #validates :date_founded, presence: true
+    validates :description, length: {minimum: 10}
 end
