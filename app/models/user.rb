0ecf9c72aa1ff812727c1c98e7e3b8f447986a18
Benis Tambe <will.jenifer@yahoo.com>
2021-06-28 21:11:14 -0400

    defined relationships in user model and added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :club_users
+    
+    has_many :clubs, through: :club_users
+    has_secure_password
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
