fdc65c9cd41d3cb044480bf2ef69ba2df7062c2b
Benis Tambe <will.jenifer@yahoo.com>
2021-06-28 21:41:26 -0400

    Fixed user/club aliasing

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,7 +2,7 @@ class Club < ApplicationRecord
     belongs_to :user #person who created it
     has_many :club_users
 
-    has_many :club_members, through: :club_users, source: :user #Those who joined
+    has_many :users, through: :club_users #Those who joined
     validates :name,  presence: true, uniqueness: true, length: {minimum: 3}
     #validates :date_founded, presence: true
     validates :description, length: {minimum: 10}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :club_users
     
     has_many :clubs, through: :club_users
+    has_many :club_memberships, through: :club_users, source: :club #Clubs they joined
+    has_many :clubs #clubs they created
     has_secure_password
 
     validates :name, presence: true
