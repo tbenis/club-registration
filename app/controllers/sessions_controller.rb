b27a090d7cf88905c27f5634368186824d4a6805
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 11:12:02 -0400

    create method for log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,21 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end
 
+    def new
+    end
+    def create  
+        @user = User.find_by(email: params[:user][:email])
+        puts "$$$$$$$$$$$$ #{params[:user][:password]}"
+       
+
+        if @user.try(:authenticate, params[:user][:password])
+            puts "%%%%%%%%%%%%%%%%%%%%"
+            # byebug
+            session[:user_id]  = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Email or password is incorrect"
+            redirect_to login_path
+        end
+    end
 end
