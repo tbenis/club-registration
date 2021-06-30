a27c2b19f31bfec159c07c3a5329a7ba0eeedc84
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 08:17:19 -0400

    welcoome route & method in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  get '/signup' => 'users#new'
+
   resources :users
   resources :clubs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
