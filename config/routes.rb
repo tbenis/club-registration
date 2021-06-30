69c961735ce222faf364f72899c085abc70ca5e0
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 15:18:48 -0400

    club_users nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,19 @@ Rails.application.routes.draw do
   resources :users do
     resources :clubs
   end
+
+  resources :clubs do
+    resources :users
+  end
+
+  resources :clubs do
+    resources :club_users
+  end
+  # resources :users do
+  #   resources :club_users, shallow: true ##shallow: true routing to avoid deep nesting
+  # end
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
