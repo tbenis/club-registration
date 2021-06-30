621b980dfb1c1ad138b7b02954c9f65235ad15d5
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 15:14:54 -0400

    addedindex  title coditionally for clubs

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,2 +1,14 @@
 module ClubsHelper
+
+    def show_index_title_conditionally
+
+        if @user 
+
+        content_tag(:h1, "Here are all of #{@user.name}'s clubs:", class: "club_index_header")
+
+        else 
+            content_tag(:h1, "Here are all existing clubs:", class: "club_index_header")
+
+        end 
+    end
 end
