e8847994602229e583642c04fca6388ceb706486
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 15:13:53 -0400

    removed  uneeded line  in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :club_users
     
-    has_many :clubs, through: :club_users
+    # has_many :clubs, through: :club_users
     has_many :club_memberships, through: :club_users, source: :club #Clubs they joined
     has_many :clubs #clubs they created
     
