d5d56edeea1d88cd7204776657f3771cf51a7922
Benis Tambe <will.jenifer@yahoo.com>
2021-06-30 08:19:46 -0400

    login routes and link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :club_users
     
     has_many :clubs, through: :club_users
     has_many :club_memberships, through: :club_users, source: :club #Clubs they joined
     has_many :clubs #clubs they created
-    has_secure_password
+    
 
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'sessions#welcome'
   get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
+
 
   resources :users
   resources :clubs
