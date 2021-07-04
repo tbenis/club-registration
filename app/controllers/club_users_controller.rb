fbac9b9b724d80b3a646df74af39e80808f55635
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 15:00:01 -0400

    create and update club users

--- app/controllers/club_users_controller.rb
+++ app/controllers/club_users_controller.rb
@@ -6,6 +6,26 @@ class ClubUsersController < ApplicationController
     @club_user = @club.club_users.build #user_id: params[:user_id]
   end
 
+  def create
+    @club_user = current_user.club_users.build(club_user_params)
+
+    if @club_user.club.user_id != current_user.id
+      if @club_user.save
+        @club_user.admin = false
+        redirect_to club_path(@club_user.club_id)
+      else
+        render :new
+      end
+    else
+      @club_user.admin = true
+      @club_user.reason = "Club Founder"
+      if @club_user.save
+        redirect_to club_path(@club_user.club_id)
+      else
+        render :new
+      end
+    end
+  end
 
   def index
     @club_users = ClubUser.all
@@ -19,6 +39,14 @@ class ClubUsersController < ApplicationController
     @club_users = ClubUser.where(club_id: params[:id])
   end
 
+  def update
+    @club_user = ClubUser.find_by_id(params[:id])
+    @club_user.admin = params[:club_user][:admin]
+    @club_user.save #(validate: false)
+    # byebug
+    redirect_to edit_club_path(@club_user.club.id)
+    # edit_club_path(@club.id)
+  end
 
   def destroy
     @club_user = ClubUser.find_by(user_id: params[:user_id])
