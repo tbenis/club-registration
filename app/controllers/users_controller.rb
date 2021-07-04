0c96715bc25ecd136f4c2ab412917cbeed8b8ead
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 14:46:27 -0400

    indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,36 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
-    end
-    def show
-        @user = User.find_by_id(params[:id])
-        redirect_to '/' if !@user
-    end
+  def new
+    redirect_to user_path(current_user.id) if logged_in?
+    @user = User.new
+  end
 
-    private
-    def user_params
-        params.require(:user).permit(:name, :email, :password)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
     end
+  end
+
+  def show
+    redirect_if_not_logged_in
+    @user = User.find_by_id(params[:id])
+    @club_users = ClubUser.where(user_id: @user.id)
+    # byebug
+    redirect_to "/" if !@user
+  end
+
+  def index
+    redirect_if_not_logged_in
+    @user = User.all
+    # redirect_to '/' if !@user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, session[:user_id])
+  end
 end
