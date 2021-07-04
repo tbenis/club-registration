343076a11b34c7fa63f5fd2d620c57f0593f548a
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 15:06:25 -0400

    validations

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,12 @@
 class Club < ApplicationRecord
-    belongs_to :user #person who created it
-    has_many :club_users
+  belongs_to :user #person who created it
+  has_many :club_users, :dependent => :destroy
+
+  has_many :users, through: :club_users, :dependent => :destroy #Those who joined
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :date_founded, presence: true
+  validate :date_founded_cannot_be_in_the_future
+  validates :description, length: { minimum: 10 }
 
   def date_founded_cannot_be_in_the_future
     if date_founded.present? && date_founded > Date.today
