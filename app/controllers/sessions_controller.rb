d12a53b4d3fc01e0ac0bbd9892da9c451753ecd7
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 14:39:37 -0400

    redirecting users to home page if logged in + omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
-    def welcome
-    end
+  def welcome
+    redirect_to user_path(current_user.id) if logged_in?
+  end
 
-    def  destroy
-        session.delete(:user_id)
-        redirect_to '/'
-    end
+  def destroy
+    session.delete(:user_id)
+    redirect_to "/"
+  end
 
-    def new
-    end
+  def new
+    redirect_to user_path(current_user.id) if logged_in?
+  end
     def create  
         @user = User.find_by(email: params[:user][:email])
         puts "$$$$$$$$$$$$ #{params[:user][:password]}"
@@ -24,4 +26,19 @@ class SessionsController < ApplicationController
             redirect_to login_path
         end
     end
+end
+
+  def omniauth
+    @user = User.create_by_google_omniauth(auth)
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
+
 end
