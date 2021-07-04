52870c8fe615a38ee4138352a166c5791870c8fa
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 15:06:05 -0400

    added custom validation cor club start date

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,8 +2,9 @@ class Club < ApplicationRecord
     belongs_to :user #person who created it
     has_many :club_users
 
-    has_many :users, through: :club_users #Those who joined
-    validates :name,  presence: true, uniqueness: true, length: {minimum: 3}
-    #validates :date_founded, presence: true
-    validates :description, length: {minimum: 10}
+  def date_founded_cannot_be_in_the_future
+    if date_founded.present? && date_founded > Date.today
+      errors.add(:date_founded, "Cannot be in the future")
+    end
+  end
 end
