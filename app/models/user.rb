4af9e12287902ece0a4f83547ac4d0c873fd899a
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 14:45:14 -0400

    indentaton

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_many :club_users
-    
-    # has_many :clubs, through: :club_users
-    has_many :club_memberships, through: :club_users, source: :club #Clubs they joined
-    has_many :clubs #clubs they created
-    
+  has_secure_password
+  has_many :club_users, :dependent => :destroy
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+  # has_many :clubs, through: :club_users
+  has_many :club_memberships, through: :club_users, source: :club, :dependent => :destroy #Clubs they joined
+  has_many :club_memberss, through: :club_users, source: :club, :dependent => :destroy #Clubs they joined
+  has_many :clubs, :dependent => :destroy #clubs they created
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
   def self.create_by_google_omniauth(auth)
     self.find_or_create_by(email: auth[:info][:email]) do |u|
