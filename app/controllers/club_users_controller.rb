1eaf75035299768f9cf59641a5f6577e8a358de3
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 13:43:04 -0400

    new action for clubusers controller

--- app/controllers/club_users_controller.rb
+++ app/controllers/club_users_controller.rb
@@ -1,5 +1,9 @@
 class ClubUsersController < ApplicationController
-    def new
-        @club_user = ClubUser.new(user_id: params[:user_id])
-    end
-end
+  before_action :redirect_if_not_logged_in
+
+  def new
+    @club = Club.find_by_id(params[:club_id]) #user_id: params[:user_id]
+    @club_user = @club.club_users.build #user_id: params[:user_id]
+  end
+
+
