06cd0fa562b006354ecac6fa66ee556cbd28874b
Benis Tambe <will.jenifer@yahoo.com>
2021-07-04 15:05:28 -0400

    cleaning code

--- app/models/club_user.rb
+++ app/models/club_user.rb
@@ -1,8 +1,10 @@
 class ClubUser < ApplicationRecord
   belongs_to :user
   belongs_to :club
+
   validates :reason, presence: true
   validate :already_a_member
+  
   def already_a_member
     if self.new_record? && @club_user = ClubUser.find_by(user_id: user_id, club_id: club_id)
       errors.add(:user_id, "#{@club_user.user.name} is already a member")
