a3a17562b030fc7a2d556963a3e0e642de4cd9dc
Benis Tambe <will.jenifer@yahoo.com>
2021-07-05 14:49:10 -0400

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_162843) do
+ActiveRecord::Schema.define(version: 2021_07_05_133456) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "club_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "club_id"
     t.boolean "admin"
+    t.string "reason"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "reason"
     t.index ["club_id"], name: "index_club_users_on_club_id"
     t.index ["user_id"], name: "index_club_users_on_user_id"
   end
 
   create_table "clubs", force: :cascade do |t|
     t.string "name"
+    t.string "summary"
     t.text "description"
     t.datetime "date_founded"
     t.integer "user_id"
@@ -41,6 +70,8 @@ ActiveRecord::Schema.define(version: 2021_07_04_162843) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "club_users", "clubs"
   add_foreign_key "club_users", "users"
   add_foreign_key "clubs", "users"
